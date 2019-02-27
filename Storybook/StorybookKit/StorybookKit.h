0541e5b03167bd97066695c800fc65183cbf4623
Yuka Kobayashi <yuka.kobayashi@eure.jp>
2019-02-27 15:15:17 +0900

    Add targets

---
+++ Storybook/StorybookKit/StorybookKit.h
@@ -0,0 +1,19 @@
+//
+//  StorybookKit.h
+//  StorybookKit
+//
+//  Created by Yuka Kobayashi on 2019/02/27.
+//  Copyright © 2019 eureka, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StorybookKit.
+FOUNDATION_EXPORT double StorybookKitVersionNumber;
+
+//! Project version string for StorybookKit.
+FOUNDATION_EXPORT const unsigned char StorybookKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorybookKit/PublicHeader.h>
+
+
