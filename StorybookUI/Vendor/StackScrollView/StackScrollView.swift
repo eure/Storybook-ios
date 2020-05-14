8daadb6200bc46e8e65e877808ebda1bc548482c
Muukii <muukii.app@gmail.com>
2020-05-14 23:37:26 +0900

    Add Lisense

--- StorybookUI/Vendor/StackScrollView/StackScrollView.swift
+++ StorybookUI/Vendor/StackScrollView/StackScrollView.swift
@@ -1,6 +1,5 @@
-// StackScrollView.swift
 //
-// Copyright (c) 2016 muukii
+// Copyright (c) 2020 Eureka, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
