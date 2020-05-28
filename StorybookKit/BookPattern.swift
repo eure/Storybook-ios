87ac0334ba1e41da74203e2e36fe40f1fa9e73b7
Muukii <muukii.app@gmail.com>
2020-05-28 22:17:16 +0900

    Add more patterns

--- StorybookKit/BookPattern.swift
+++ StorybookKit/BookPattern.swift
@@ -88,6 +88,43 @@ extension BookPattern where Element == Any {
 
     return .init(buffer)
 
+  }
+
+  public static func make<P0: Sequence, P1: Sequence, P2: Sequence, P3: Sequence>(_ p0: P0, _ p1: P1, _ p2: P2, _ p3: P3) -> BookPattern<(P0.Element, P1.Element, P2.Element, P3.Element)> {
+
+    var buffer: [(P0.Element, P1.Element, P2.Element, P3.Element)] = []
+
+    p0.forEach { p0 in
+      p1.forEach { p1 in
+        p2.forEach { p2 in
+          p3.forEach { p3 in
+            buffer.append((p0, p1, p2, p3))
+          }
+        }
+      }
+    }
+
+    return .init(buffer)
+
+  }
+
+  public static func make<P0: Sequence, P1: Sequence, P2: Sequence, P3: Sequence, P4: Sequence>(_ p0: P0, _ p1: P1, _ p2: P2, _ p3: P3, _ p4: P4) -> BookPattern<(P0.Element, P1.Element, P2.Element, P3.Element, P4.Element)> {
+
+    var buffer: [(P0.Element, P1.Element, P2.Element, P3.Element, P4.Element)] = []
+
+    p0.forEach { p0 in
+      p1.forEach { p1 in
+        p2.forEach { p2 in
+          p3.forEach { p3 in
+            p4.forEach { p4 in
+              buffer.append((p0, p1, p2, p3, p4))
+            }
+          }
+        }
+      }
+    }
+
+    return .init(buffer)
 
   }
 
