7c2a40b45c0dacf8519dd263043ddb6d0f45fff3
Muukii <muukii.app@gmail.com>
2022-01-15 14:58:37 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Storybook",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v12),
     ],
     products: [
         .library(name: "StorybookKit", targets: ["StorybookKit"]),
