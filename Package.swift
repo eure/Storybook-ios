735a24f0b9ca8c1ae4daac7a36fa3f6a420d1ad1
Muukii <muukii.app@gmail.com>
2022-07-05 18:16:12 +0900

    Update Package.swift - add Texture dependency #28

--- Package.swift
+++ Package.swift
@@ -2,27 +2,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "Storybook",
-    platforms: [
-        .iOS(.v12),
-    ],
-    products: [
-        .library(name: "StorybookKit", targets: ["StorybookKit"]),
-        .library(name: "StorybookUI", targets: ["StorybookUI"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/muukii/MondrianLayout.git", from: "0.8.0")
-    ],
-    targets: [
-        .target(
-            name: "StorybookKit",
-            dependencies: [],
-            path: "StorybookKit"
-        ),
-        .target(
-            name: "StorybookUI",
-            dependencies: ["StorybookKit", "MondrianLayout"],
-            path: "StorybookUI"
-        )
-    ]
+  name: "Storybook",
+  platforms: [
+    .iOS(.v12)
+  ],
+  products: [
+    .library(name: "StorybookKit", targets: ["StorybookKit"]),
+    .library(name: "StorybookKitTextureSupport", targets: ["StorybookKitTextureSupport"]),
+    .library(name: "StorybookUI", targets: ["StorybookUI"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/muukii/MondrianLayout.git", from: "0.8.0"),
+    .package(url: "https://github.com/FluidGroup/TextureBridging.git", branch: "main"),
+    .package(url: "https://github.com/FluidGroup/TextureSwiftSupport.git", branch: "main"),
+  ],
+  targets: [
+    .target(
+      name: "StorybookKit",
+      dependencies: [],
+      path: "StorybookKit"
+    ),
+    .target(
+      name: "StorybookKitTextureSupport",
+      dependencies: [
+        .product(name: "TextureSwiftSupport", package: "TextureSwiftSupport"),
+        .product(name: "TextureBridging", package: "TextureBridging"),
+        "StorybookKit",
+      ],
+      path: "StorybookKitTextureSupport"
+    ),
+    .target(
+      name: "StorybookUI",
+      dependencies: ["StorybookKit", "MondrianLayout"],
+      path: "StorybookUI"
+    ),
+  ]
 )
