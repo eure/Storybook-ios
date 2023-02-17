dd6b71922de9affe21dbe7d82722ed7ebe50ba07
Muukii <muukii.app@gmail.com>
2023-02-17 15:19:34 +0900

    Patch

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Storybook",
   platforms: [
-    .iOS(.v12)
+    .iOS(.v13)
   ],
   products: [
     .library(name: "StorybookKit", targets: ["StorybookKit"]),
