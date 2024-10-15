d845beed9d1e4983ccec7d8228775f5b1da0e25a
Muukii <muukii.app@gmail.com>
2024-10-15 17:54:55 +0900

    Patch

--- Package.swift
+++ Package.swift
@@ -1,26 +1,26 @@
-// swift-tools-version:5.9
-import PackageDescription
+// swift-tools-version:6.0
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
   name: "Storybook",
   platforms: [
     .iOS(.v15),
     .macCatalyst(.v15),
-    .macOS(.v10_15)
+    .macOS(.v10_15),
   ],
   products: [
     .library(name: "StorybookKit", targets: ["StorybookKit"]),
-    .library(name: "StorybookKitTextureSupport", targets: ["StorybookKitTextureSupport"]),
+//    .library(name: "StorybookKitTextureSupport", targets: ["StorybookKitTextureSupport"]),
   ],
   dependencies: [
     .package(url: "https://github.com/muukii/MondrianLayout.git", from: "0.8.0"),
-    .package(url: "https://github.com/FluidGroup/TextureBridging.git", branch: "main"),
-    .package(url: "https://github.com/FluidGroup/TextureSwiftSupport.git", branch: "main"),
+    .package(url: "https://github.com/FluidGroup/TextureBridging.git", from: "3.2.1"),
+    .package(url: "https://github.com/FluidGroup/TextureSwiftSupport.git", from: "3.20.1"),
     .package(url: "https://github.com/FluidGroup/swiftui-support", from: "0.4.1"),
     .package(url: "https://github.com/FluidGroup/ResultBuilderKit", from: "1.3.0"),
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", branch: "main")
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
+    .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", from: "0.5.2"),
   ],
   targets: [
     .target(
@@ -28,14 +28,14 @@ let package = Package(
       dependencies: [
         "StorybookMacrosPlugin",
         .product(name: "SwiftUISupport", package: "swiftui-support"),
-        "ResultBuilderKit"
+        "ResultBuilderKit",
       ]
     ),
     .macro(
       name: "StorybookMacrosPlugin",
       dependencies: [
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
       ]
     ),
     .testTarget(
@@ -46,13 +46,13 @@ let package = Package(
         .product(name: "MacroTesting", package: "swift-macro-testing"),
       ]
     ),
-    .target(
-      name: "StorybookKitTextureSupport",
-      dependencies: [
-        .product(name: "TextureSwiftSupport", package: "TextureSwiftSupport"),
-        .product(name: "TextureBridging", package: "TextureBridging"),
-        "StorybookKit",
-      ]
-    )
+//    .target(
+//      name: "StorybookKitTextureSupport",
+//      dependencies: [
+//        .product(name: "TextureSwiftSupport", package: "TextureSwiftSupport"),
+//        .product(name: "TextureBridging", package: "TextureBridging"),
+//        "StorybookKit",
+//      ]
+//    ),
   ]
 )
